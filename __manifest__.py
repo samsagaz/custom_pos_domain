eb66ca54aefb895f4c9c21a528b6799cf32e599e
Jose Luis Zanotti <joseluiszanotti@gmail.com>
2023-08-24 12:38:53 -0300

    [IMP] Initial Commit

---
+++ __manifest__.py
@@ -0,0 +1,23 @@
+{
+    "name": "Product POS custom domain",
+    "summary": """
+        Product POS custom domain""",
+    "description": """
+        Product POS custom domain
+    """,
+    "license": "LGPL-3",
+    "author": "Jose Luis Zanotti",
+    "website": "",
+    "category": "Customizations",
+    "version": "15.0.1.0.1",
+    "depends": [
+        "point_of_sale",
+    ],
+    'assets': {
+        'point_of_sale.assets': [
+            'custom_pos_domain/static/src/js/models.js',
+        ],
+    },
+    "data": [
+    ],
+}
